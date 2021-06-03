d52afd2ba42b854fdc3719dc7384e3f70cbe2c03
viveknathani <viveknathani2402@gmail.com>
2021-06-03 20:00:24 +0530

    feat: use Home component
    This commit puts the home component into use. It also rectifies a
    mistake made earlier. The file extension should be .tsx and not .jsx.

---
+++ src/components/app/app.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import Home from '../home/home';
+
+function App() : JSX.Element {
+  return (
+    <div className="App">
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
