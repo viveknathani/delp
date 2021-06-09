1a0544ecc6b39d029c667223861b97287283c5d6
viveknathani <viveknathani2402@gmail.com>
2021-06-09 18:10:16 +0530

    test: wrap tests using describe()
    describe() provides a neat separation and logs well to the console too.

--- src/components/strstats/core.test.ts
+++ src/components/strstats/core.test.ts
@@ -2,58 +2,63 @@ import * as strstats from './core';
 
 // Begin: strstats.totalWords
 
-it('totalWords: Should work on a normal text.', () => {
-    expect(strstats.totalWords('This is normal text.')).toEqual(4);
-});
-
-it('totalWords: Should work on empty text.', () => {
-    expect(strstats.totalWords(' ')).toEqual(0);
-});
-
-it('totalWords: Should work on text with emojis.', () => {
-    expect(strstats.totalWords('this is random text 🚀')).toEqual(5);
-});
-
-it('totalWords: Should work on text with emojis only.', () => {
-    expect(strstats.totalWords('👍👀 🚀 💯')).toEqual(3);
+describe('totalWords()', () => {
+    it('Should work on a normal text.', () => {
+        expect(strstats.totalWords('This is normal text.')).toEqual(4);
+    });
+    
+    it('Should work on empty text.', () => {
+        expect(strstats.totalWords(' ')).toEqual(0);
+    });
+    
+    it('Should work on text with emojis.', () => {
+        expect(strstats.totalWords('this is random text 🚀')).toEqual(5);
+    });
+    
+    it('Should work on text with emojis only.', () => {
+        expect(strstats.totalWords('👍👀 🚀 💯')).toEqual(3);
+    });
 });
 
 // End: strstats.totalWords
 
 // Begin: strstats.getWordDistribution
 
-it('getWordDistribution: Should work on a normal text.', () => {
+describe('getWordDistribution()', () => {
 
-    const table: Map<String, number> = new Map();
-    const text: String = new String('This is normal text.');
+    let table: Map<String, number>;
+    beforeEach(() => {
+        table = new Map();
+    });
 
-    table.set('This', 1);
-    table.set('is', 1);
-    table.set('normal', 1);
-    table.set('text.', 1);
+    it('Should work on a normal text.', () => {
+        const text: String = new String('This is normal text.');    
 
-    expect(strstats.getWordDistribution(text)).toEqual(table);
-});
-
-it('getWordDistribution: Should work on empty text.', () => {
-
-    const table: Map<String, number> = new Map();
-    const text: String = new String(' ');
-
-    expect(strstats.getWordDistribution(text)).toEqual(table);
-});
-
-it('getWordDistribution: Should work with emojis.', () => {
-
-    const table: Map<String, number> = new Map();
-    const text: String = new String('👍 👀 👀 🚀 💯');
-
-    table.set('👍', 1);
-    table.set('👀', 2);
-    table.set('🚀', 1);
-    table.set('💯', 1);
-
-    expect(strstats.getWordDistribution(text)).toEqual(table);
+        table.set('This', 1);
+        table.set('is', 1);
+        table.set('normal', 1);
+        table.set('text.', 1);
+    
+        expect(strstats.getWordDistribution(text)).toEqual(table);
+    });
+    
+    it('Should work on empty text.', () => {
+    
+        const text: String = new String(' ');
+        expect(strstats.getWordDistribution(text)).toEqual(table);
+    });
+    
+    it('Should work with emojis.', () => {
+    
+        const text: String = new String('👍 👀 👀 🚀 💯');
+    
+        table.set('👍', 1);
+        table.set('👀', 2);
+        table.set('🚀', 1);
+        table.set('💯', 1);
+    
+        expect(strstats.getWordDistribution(text)).toEqual(table);
+    });
 });
 
 // End: strstats.getWordDistribution
