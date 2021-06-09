8c0175107db9978872d2e2652198cbb755d074a4
viveknathani <viveknathani2402@gmail.com>
2021-06-09 17:50:49 +0530

    feat: find distribution of words
    We use a map data structure for this. Default map insertion is not very
    flexible for our use case so we use a custom function to do so.

--- src/components/strstats/core.ts
+++ src/components/strstats/core.ts
@@ -25,6 +25,55 @@ const totalWords = (input: String, separator: String = ' '): number => {
     return result;
 }
 
+/**
+ * A function to perform map insertion, in a better way than Map.prototype.set
+ * This is written specifically for the use case of mapping a word to its frequency.
+ * @param table The map where insertion should take place
+ * @param word  The word to insert
+ * @returns     Nothing
+ */
+const insertIntoMap = (table: Map<String, number>, word: String): void => {
+    if (word === ' ' || word === undefined) {
+        return;
+    }
+
+    if (table.has(word)) {
+        let currentValue: number = table.get(word) || 0;
+        table.set(word, currentValue + 1);
+    }
+    else {
+        table.set(word, 1);
+    }
+}
+
+/**
+ * Get a map of words with their occurence count in a given text.
+ * @param input     The given text
+ * @param separator About which characters are distinguished as words, default is whitespace
+ * @returns          The frequency map
+ */
+const getWordDistribution = (input: String, separator: String = ' '): Map<String, number> => {
+    
+    const table: Map<String, number> = new Map();
+    let word: string = '';
+
+    for (let i = 0; i < input.length; ++i) {
+
+        if (input[i] === separator && word !== '') {
+            insertIntoMap(table, word)
+            word = '';
+            continue;
+        }
+
+        word += input[i];
+    }
+
+    insertIntoMap(table, word);
+    return table;
+}
+
+
 export {
-    totalWords
+    totalWords,
+    getWordDistribution
 }
