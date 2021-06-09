0f8f945c76c0d60bdf0c30ec9b503827576bc812
viveknathani <viveknathani2402@gmail.com>
2021-06-09 17:52:16 +0530

    test: check for word distribution

--- src/components/strstats/core.test.ts
+++ src/components/strstats/core.test.ts
@@ -19,3 +19,41 @@ it('totalWords: Should work on text with emojis only.', () => {
 });
 
 // End: strstats.totalWords
+
+// Begin: strstats.getWordDistribution
+
+it('getWordDistribution: Should work on a normal text.', () => {
+
+    const table: Map<String, number> = new Map();
+    const text: String = new String('This is normal text.');
+
+    table.set('This', 1);
+    table.set('is', 1);
+    table.set('normal', 1);
+    table.set('text.', 1);
+
+    expect(strstats.getWordDistribution(text)).toEqual(table);
+});
+
+it('getWordDistribution: Should work on empty text.', () => {
+
+    const table: Map<String, number> = new Map();
+    const text: String = new String(' ');
+
+    expect(strstats.getWordDistribution(text)).toEqual(table);
+});
+
+it('getWordDistribution: Should work with emojis.', () => {
+
+    const table: Map<String, number> = new Map();
+    const text: String = new String('👍 👀 👀 🚀 💯');
+
+    table.set('👍', 1);
+    table.set('👀', 2);
+    table.set('🚀', 1);
+    table.set('💯', 1);
+
+    expect(strstats.getWordDistribution(text)).toEqual(table);
+});
+
+// End: strstats.getWordDistribution
